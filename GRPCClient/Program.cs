4db95a6bf4706bf6f6f0aebdfe210484cf4eb780
Angela Yang <31804752+angelagyang@users.noreply.github.com>
2020-07-09 18:45:38 -0400

    Update client certificate configuration

--- GRPCClient/Program.cs
+++ GRPCClient/Program.cs
@@ -1,8 +1,9 @@
-﻿
+
 using Grpc.Core;
 using GRPCServer;
 using ProtoBuf.Grpc.Client;
 using System;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
@@ -10,13 +11,18 @@ namespace GRPCClient
 {
     class Program
     {
+        private static string _sslThumbprint = ""; // Your own SSL certificate thumbprint 
+
         static void Main(string[] args)
         {
-            // Where can I configure a client certificate? 
-            var channelCreds = new SslCredentials(GetRootCertificates());
+            // Using sample test credentials from https://github.com/grpc/grpc/tree/master/src/core/tsi/test_creds
+            string rootDir = $"{Directory.GetCurrentDirectory()}/../.."; 
+            var keyCertPair = new KeyCertificatePair(File.ReadAllText($"{rootDir}/samplecert.pem.txt"), File.ReadAllText($"{rootDir}/samplecert.key.txt")); 
+            var channelCreds = new SslCredentials(GetRootCertificates(), keyCertPair);
+            
             var channel = new Channel("localhost", 5001, channelCreds);
-            var greeter = channel.CreateGrpcService<IGreeterService>();
 
+            var greeter = channel.CreateGrpcService<IGreeterService>();
             try
             {
                 Console.WriteLine(greeter.Ping()); 
@@ -27,24 +33,30 @@ namespace GRPCClient
             Console.ReadLine(); 
         }
 
+        private static X509Certificate2 LoadSSLCertificate()
+        {
+            var certStore = new X509Store(StoreLocation.LocalMachine);
+            certStore.Open(OpenFlags.ReadOnly);
+            var cert = certStore.Certificates.Find(X509FindType.FindByThumbprint, _sslThumbprint, true)[0];
+            certStore.Close();
+
+            return cert;
+        }
+
         /**
         * Source: https://stackoverflow.com/questions/58125102/grpc-net-client-fails-to-connect-to-server-with-ssl
         **/
         public static string GetRootCertificates()
         {
             StringBuilder builder = new StringBuilder();
-            X509Store store = new X509Store(StoreName.Root);
-            store.Open(OpenFlags.ReadOnly);
-            foreach (X509Certificate2 mCert in store.Certificates)
-            {
-                builder.AppendLine(
-                    "# Issuer: " + mCert.Issuer.ToString() + "\n" +
-                    "# Subject: " + mCert.Subject.ToString() + "\n" +
-                    "# Label: " + mCert.FriendlyName.ToString() + "\n" +
-                    "# Serial: " + mCert.SerialNumber.ToString() + "\n" +
-                    "# SHA1 Fingerprint: " + mCert.GetCertHashString().ToString() + "\n" +
-                    ExportToPEM(mCert) + "\n");
-            }
+            var cert = LoadSSLCertificate(); 
+            builder.AppendLine(
+                    "# Issuer: " + cert.Issuer.ToString() + "\n" +
+                    "# Subject: " + cert.Subject.ToString() + "\n" +
+                    "# Label: " + cert.FriendlyName.ToString() + "\n" +
+                    "# Serial: " + cert.SerialNumber.ToString() + "\n" +
+                    "# SHA1 Fingerprint: " + cert.GetCertHashString().ToString() + "\n" +
+                    ExportToPEM(cert) + "\n");
             return builder.ToString();
         }
 
