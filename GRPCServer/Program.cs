3974087bade86d396b79f55b07404ca03fa9db62
Angela Yang <31804752+angelagyang@users.noreply.github.com>
2020-07-09 18:48:01 -0400

    Update server with sample cert validation

--- GRPCServer/Program.cs
+++ GRPCServer/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,7 +12,10 @@ namespace GRPCServer
 {
     class Program
     {
-        private static string _thumbprint = ""; // Thumbprint for Client Cert Verification
+        // Using sample test credentials from https://github.com/grpc/grpc/tree/master/src/core/tsi/test_creds
+        private static string _sslThumbprint = ""; // Your own SSL certificate (for localhost) thumbprint 
+        private static string _clientThumbprint = "BC0C1B5DAC867DB1B5502CA60539569C75F342C4"; // Thumbprint from sample cert 
+
         static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -22,21 +26,32 @@ namespace GRPCServer
             return WebHost.CreateDefaultBuilder(args)
                 .UseKestrel(o =>
                 {
+                    var sslCertificate = LoadSSLCertificate(); 
                     o.ListenAnyIP(5001, listenOptions =>
                     {
-                        listenOptions.UseHttps(httpsOptions =>
+                        listenOptions.UseHttps(sslCertificate, httpsOptions =>
                         {
                             httpsOptions.SslProtocols = SslProtocols.Tls12;
-                            httpsOptions.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+                            httpsOptions.ClientCertificateMode = ClientCertificateMode.NoCertificate;
                             httpsOptions.ClientCertificateValidation = (certificate, chain, errors) =>
                             {
-                                return certificate.Thumbprint.Equals(_thumbprint, StringComparison.OrdinalIgnoreCase);
+                                return certificate.Thumbprint.Equals(_clientThumbprint, StringComparison.OrdinalIgnoreCase);
                             };
                         });
                     });
                 })
                 .UseUrls("https://localhost:5001")
                 .UseStartup<Program>();
+        }
+
+        private static X509Certificate2 LoadSSLCertificate()
+        {
+            var certStore = new X509Store(StoreLocation.LocalMachine);
+            certStore.Open(OpenFlags.ReadOnly);
+            var cert = certStore.Certificates.Find(X509FindType.FindByThumbprint, _sslThumbprint, true)[0];
+            certStore.Close();
+
+            return cert;
         }
 
         public void ConfigureServices(IServiceCollection services)
